37c15252ae9f5444fb8c9f83d232d500802862ce
Yuuki Shimizu <yuuki@maxio.jp>
2019-05-24 11:41:27 +0900

    Change: use AndroidX

--- app/src/main/java/com/github/yuukis/mtfuji_locator/MainActivity.kt
+++ app/src/main/java/com/github/yuukis/mtfuji_locator/MainActivity.kt
@@ -1,6 +1,6 @@
 package com.github.yuukis.mtfuji_locator
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
