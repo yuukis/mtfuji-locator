addfe7d7a3913ac81ed8e74f80bfc2a002d2b94a
Yuuki Shimizu <yuuki@maxio.jp>
2019-05-25 03:07:42 +0900

    Change: do not show results if sensor data is not enough

--- app/src/main/java/com/github/yuukis/mtfuji_locator/MainActivity.kt
+++ app/src/main/java/com/github/yuukis/mtfuji_locator/MainActivity.kt
@@ -30,9 +30,9 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
     var sensorManager: SensorManager? = null
     var bestProvider: String? = null
     var currentLocation: Location? = null
-    var distanceToMtFuji: Float = 0f
-    var azimuthToMtFuji: Int = 0
-    var azimuthBySensor: Int = 0
+    var distanceToMtFuji: Float? = null
+    var azimuthToMtFuji: Int? = null
+    var azimuthBySensor: Int? = null
     var accelerometerValues: FloatArray? = null
     var magneticFieldValues: FloatArray? = null
 
@@ -146,7 +146,8 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
         }
     }
 
-    fun showResult(distance: Float, azimuth: Int, sensorAzimuth: Int) {
+    fun showResult(distance: Float?, azimuth: Int?, sensorAzimuth: Int?) {
+        if (distance == null || azimuth == null || sensorAzimuth == null) return
         val emojiView = findViewById<TextView>(R.id.emoji)
         val textView = findViewById<TextView>(R.id.text1)
         var azimuthDelta = sensorAzimuth - azimuth
@@ -161,7 +162,7 @@ class MainActivity : AppCompatActivity(), LocationListener, SensorEventListener 
             azimuthDelta > 15 -> "⬅️"
             else -> "🗻"
         }
-        textView.text = "${distanceKm.toString()} km"
+        textView.text = "${distanceKm} km"
     }
 
     private fun calcDistance(x0: Float, y0: Float): Float {
