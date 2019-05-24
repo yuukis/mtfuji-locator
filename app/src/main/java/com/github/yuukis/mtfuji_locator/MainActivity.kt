bc82817bd3252a8131dcf3d94f841d4905c4fd32
Yuuki Shimizu <yuuki@maxio.jp>
2019-05-24 13:20:11 +0900

    Add: show permission request on starting activity

--- app/src/main/java/com/github/yuukis/mtfuji_locator/MainActivity.kt
+++ app/src/main/java/com/github/yuukis/mtfuji_locator/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.github.yuukis.mtfuji_locator
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import permissions.dispatcher.NeedsPermission
+import permissions.dispatcher.RuntimePermissions
 
+@RuntimePermissions
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initLocationManagerWithPermissionCheck()
     }
+
+    @NeedsPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+    fun initLocationManager() {
+    }
+
+
 }
